1b4538931f4c1be81920ed4d898b5577c3cecbb2
Madmax6333 <Madmax6333@DESKTOP-O0N06LS>
2022-11-13 19:18:30 +0000

    Fixed Rhodium Ore Drop

--- src/main/java/net/mcreator/mod/block/RhodiumOreBlock.java
+++ src/main/java/net/mcreator/mod/block/RhodiumOreBlock.java
@@ -1,6 +1,7 @@
 
 package net.mcreator.mod.block;
 
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -8,8 +9,14 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.core.BlockPos;
+
+import net.mcreator.mod.init.ModModItems;
+
+import java.util.List;
+import java.util.Collections;
 
 public class RhodiumOreBlock extends Block {
 	public RhodiumOreBlock() {
@@ -26,5 +33,13 @@ public class RhodiumOreBlock extends Block {
 		if (player.getInventory().getSelected().getItem() instanceof TieredItem tieredItem)
 			return tieredItem.getTier().getLevel() >= 3;
 		return false;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(ModModItems.RHODIUMINGOT.get()));
 	}
 }
